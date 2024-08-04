ec9df576e930b41881945b0482533af7e9cd907f
Luis Shrestha <2echestha11@gmail.com>
2024-08-04 12:01:00 +0545

    add search bar but not the search function

--- lib/userScreen/main_screen/explore_screen.dart
+++ lib/userScreen/main_screen/explore_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:shelf/userScreen/book_page/genre_view/exploreCollegeBooks.dart';
 import 'package:shelf/userScreen/book_page/genre_view/explore_books.dart';
 import '../../../../utility/constant/constant.dart' as constant;
 import '../../utility/customBackground/abstract_background.dart';
 import '../../utility/customBackground/bottom_circular_clipper.dart';
+import '../../utility/widget/form_widget/custom_search_bar.dart';
 import '../model/repository/upload_repo/fetch_data.dart';
 
 class ExploreScreen extends StatefulWidget {
@@ -14,15 +16,37 @@ class ExploreScreen extends StatefulWidget {
 }
 
 class _ExploreScreenState extends State<ExploreScreen> {
-
   final FetchData fetchData = FetchData();
   late Future<Map<String, List<Map<String, dynamic>>>> dataFuture;
   bool showExploreBooks = true;
+
+  final ScrollController _scrollController = ScrollController();
+  bool _isScrolledDown = false;
+
+  TextEditingController searchController = TextEditingController();
+  FocusNode _searchFocus = FocusNode();
 
   @override
   void initState() {
     super.initState();
     dataFuture = fetchData.fetchAndGroupData();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+      if (!_isScrolledDown) {
+        setState(() {
+          _isScrolledDown = true;
+        });
+      }
+    } else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+      if (_isScrolledDown) {
+        setState(() {
+          _isScrolledDown = false;
+        });
+      }
+    }
   }
 
   Future<void> _refreshData() async {
@@ -35,6 +59,14 @@ class _ExploreScreenState extends State<ExploreScreen> {
     setState(() {
       showExploreBooks = !showExploreBooks;
     });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    searchController.dispose();
+    _searchFocus.dispose();
+    super.dispose();
   }
 
   @override
@@ -69,12 +101,36 @@ class _ExploreScreenState extends State<ExploreScreen> {
                 ),
               ),
             ),
-            SingleChildScrollView(
+            CustomScrollView(
+              controller: _scrollController,
               physics: const AlwaysScrollableScrollPhysics(),
-              child: Container(
-                width: double.infinity,
-                child: showExploreBooks ? ExploreBooks() : ExploreCollegeBooks(),
-              ),
+              slivers: <Widget>[
+                SliverAppBar(
+                  floating: true,
+                  pinned: false,
+                  snap: true,
+                  backgroundColor: constant.primaryColor,
+                  flexibleSpace: _isScrolledDown
+                      ? SizedBox.shrink()
+                      : Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CustomSearchBar(
+                      controller: searchController,
+                      labelText: 'Search Books',
+                      prefixIcon: Icons.search,
+                      focusNode: _searchFocus,
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Container(
+                    width: double.infinity,
+                    child: showExploreBooks
+                        ? ExploreBooks()
+                        : ExploreCollegeBooks(),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
---
+++ lib/utility/widget/form_widget/custom_search_bar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final String labelText;
+  final IconData prefixIcon;
+  final FocusNode focusNode;
+
+  const CustomSearchBar({super.key,
+    required this.controller,
+    required this.labelText,
+    required this.prefixIcon,
+    required this.focusNode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 40,
+      child: TextField(
+        textAlign: TextAlign.left,
+        controller: controller,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: TextStyle(
+              color: Colors.black87
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide(color: Colors.black54),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: const BorderSide(
+                color: Colors.black54, width: 0.6),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+                color: Colors.black54, width: 0.6),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          prefixIcon: Icon(prefixIcon),
+        ),
+
+      ),
+    );
+  }
+}
