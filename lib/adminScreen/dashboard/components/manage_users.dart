67ffe413bb9c6c5f6107d59497b4136823af6e87
Luis Shrestha <2echestha11@gmail.com>
2024-08-30 13:27:08 +0545

    updated some code in manage_users.dart and see_all_books.dart and also add new plugins

--- lib/adminScreen/dashboard/components/manage_users.dart
+++ lib/adminScreen/dashboard/components/manage_users.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 import '../../../../supports/applog/applog.dart';
 import '../../../../utility/constant/constant.dart' as constant;
 import '../../main/components/side_menu.dart';
@@ -16,12 +17,12 @@ class _ManageUsersViewState extends State<ManageUsersView> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  late Future<QuerySnapshot<Map<String, dynamic>>?> _usersData;
+  late Future<List<UserData>> _usersData;
 
-  Future<QuerySnapshot<Map<String, dynamic>>?> fetchAllUsersData() async {
+  Future<List<UserData>> fetchAllUsersData() async {
     final usersData = await _firestore.collection('users').get();
     AppLog.i("Users Data", "${usersData.docs.length} users fetched");
-    return usersData;
+    return usersData.docs.map((doc) => UserData.fromFirestore(doc)).toList();
   }
 
   @override
@@ -61,101 +62,191 @@ class _ManageUsersViewState extends State<ManageUsersView> {
       drawer: SideMenu(),
       body: RefreshIndicator(
         onRefresh: _refreshData,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: usersDataTable(),
-          ),
+        child: FutureBuilder<List<UserData>>(
+          future: _usersData,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No users found'));
+            }
+
+            final List<UserData> usersData = snapshot.data!;
+            final UserDataSource _userDataSource = UserDataSource(
+              users: usersData,
+              onDeleteUser: deleteUser,
+              onRefreshData: _refreshData,
+            );
+
+            return SfDataGrid(
+              source: _userDataSource,
+              columnWidthMode: ColumnWidthMode.fill,
+              allowColumnsDragging: true,
+              columns: <GridColumn>[
+                GridColumn(
+                  columnName: 'userPhoto',
+                  label: Container(
+                    padding: EdgeInsets.all(8.0),
+                    alignment: Alignment.center,
+                    child: Text('Photo', style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+                GridColumn(
+                  columnName: 'name',
+                  label: Container(
+                    padding: EdgeInsets.all(8.0),
+                    alignment: Alignment.center,
+                    child: Text('Name', style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+                GridColumn(
+                  columnName: 'email',
+                  label: Container(
+                    padding: EdgeInsets.all(8.0),
+                    alignment: Alignment.center,
+                    child: Text('Email', style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+                GridColumn(
+                  columnName: 'contact',
+                  label: Container(
+                    padding: EdgeInsets.all(8.0),
+                    alignment: Alignment.center,
+                    child: Text('Contact', style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+                GridColumn(
+                  columnName: 'bio',
+                  label: Container(
+                    padding: EdgeInsets.all(8.0),
+                    alignment: Alignment.center,
+                    child: Text('Bio', style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+                GridColumn(
+                  columnName: 'actions',
+                  label: Container(
+                    padding: EdgeInsets.all(8.0),
+                    alignment: Alignment.center,
+                    child: Text('Actions', style: TextStyle(color: Colors.black)),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
   }
+}
 
-  Widget usersDataTable() {
-    return FutureBuilder<QuerySnapshot<Map<String, dynamic>>?>(
-      future: _usersData,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        } else if (!snapshot.hasData || snapshot.data == null || snapshot.data!.docs.isEmpty) {
-          return const Center(child: Text('No users found'));
-        }
+class UserDataSource extends DataGridSource {
+  final Future<void> Function(String userID) onDeleteUser;
+  final Future<void> Function() onRefreshData;
 
-        final List<QueryDocumentSnapshot<Map<String, dynamic>>> usersDocs = snapshot.data!.docs;
+  UserDataSource({
+    required List<UserData> users,
+    required this.onDeleteUser,
+    required this.onRefreshData,
+  }) {
+    _userDataGridRows = users
+        .map<DataGridRow>((data) => DataGridRow(cells: [
+      DataGridCell<String>(columnName: 'userPhoto', value: data.userPhoto),
+      DataGridCell<String>(columnName: 'name', value: data.name),
+      DataGridCell<String>(columnName: 'email', value: data.email),
+      DataGridCell<String>(columnName: 'contact', value: data.contact),
+      DataGridCell<String>(columnName: 'bio', value: data.bio),
+      DataGridCell<Widget>(
+        columnName: 'actions',
+        value: IconButton(
+          icon: const Icon(Icons.delete, color: Colors.black),
+          onPressed: () async {
+            await onDeleteUser(data.id); // Call deleteUser function
+            await onRefreshData(); // Refresh the data after deletion
+          },
+        ),
+      ),
+    ]))
+        .toList();
+  }
 
-        return Expanded(
-          child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: SizedBox(
-              child: DataTable(
-                dataRowMaxHeight: 60,
-                columnSpacing: 8.0,
-                headingRowColor: WidgetStateColor.resolveWith(
-                      (states) => constant.kBackGroundColor,
-                ),
-                columns: const [
-                  DataColumn(label: Text('Photo', style: TextStyle(color: Colors.black))),
-                  DataColumn(label: Text('Name', style: TextStyle(color: Colors.black))),
-                  DataColumn(label: Text('Email', style: TextStyle(color: Colors.black))),
-                  DataColumn(label: Text('Contact', style: TextStyle(color: Colors.black))),
-                  DataColumn(label: Text('Bio', style: TextStyle(color: Colors.black))),
-                  DataColumn(label: Text('Actions', style: TextStyle(color: Colors.black))),
-                ],
-                rows: usersDocs.map((userDoc) {
-                  final Map<String, dynamic> userData = userDoc.data();
-                  final String userId = userDoc.id;
-                  return DataRow(
-                    color: WidgetStateProperty.resolveWith<Color?>(
-                          (Set<WidgetState> states) {
-                        if (states.contains(WidgetState.selected)) {
-                          return Colors.blue.withOpacity(0.08);
-                        }
-                        return Colors.grey[200]; // Alternating row color.
-                      },
-                    ),
-                    cells: [
-                      DataCell(
-                        Image.network(
-                          userData['userPhoto'] ?? '',
-                          width: 50,
-                          height: 50,
-                          errorBuilder: (context, error, stackTrace) {
-                            return const Icon(Icons.error,
-                                color: Colors.black);
-                          },
-                        ),
-                      ),
-                      DataCell(Text(userData['name'] ?? '', style: const TextStyle(color: Colors.black))),
-                      DataCell(Text(userData['email'] ?? '', style: const TextStyle(color: Colors.black))),
-                      DataCell(Text(userData['contact'] ?? '', style: const TextStyle(color: Colors.black))),
-                      DataCell(Text(userData['bio'] ?? '', style: const TextStyle(color: Colors.black))),
-                      DataCell(
-                        Row(
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.delete, color: Colors.black),
-                              onPressed: () async {
-                                if (userId.isNotEmpty) {
-                                  await deleteUser(userId);
-                                  _refreshData();
-                                } else {
-                                  print('Error: User ID is null or empty');
-                                }
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                }).toList(),
-              ),
-            ),
-          ),
-        );
-      },
+  List<DataGridRow> _userDataGridRows = [];
+
+  @override
+  List<DataGridRow> get rows => _userDataGridRows;
+
+  @override
+  DataGridRowAdapter buildRow(DataGridRow row) {
+    return DataGridRowAdapter(cells: [
+      Container(
+        padding: EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Image.network(
+          row.getCells()[0].value ?? '',
+          width: 50,
+          height: 50,
+          errorBuilder: (context, error, stackTrace) {
+            return const Icon(Icons.error, color: Colors.black);
+          },
+        ),
+      ),
+      Container(
+        padding: EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Text(row.getCells()[1].value ?? '', style: const TextStyle(color: Colors.black)),
+      ),
+      Container(
+        padding: EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Text(row.getCells()[2].value ?? '', style: const TextStyle(color: Colors.black)),
+      ),
+      Container(
+        padding: EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Text(row.getCells()[3].value ?? '', style: const TextStyle(color: Colors.black)),
+      ),
+      Container(
+        padding: EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: Text(row.getCells()[4].value ?? '', style: const TextStyle(color: Colors.black)),
+      ),
+      Container(
+        padding: EdgeInsets.all(8.0),
+        alignment: Alignment.center,
+        child: row.getCells()[5].value,
+      ),
+    ]);
+  }
+}
+
+class UserData {
+  final String id;
+  final String userPhoto;
+  final String name;
+  final String email;
+  final String contact;
+  final String bio;
+
+  UserData({
+    required this.id,
+    required this.userPhoto,
+    required this.name,
+    required this.email,
+    required this.contact,
+    required this.bio,
+  });
+
+  factory UserData.fromFirestore(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data()!;
+    return UserData(
+      id: doc.id,
+      userPhoto: data['userPhoto'] ?? '',
+      name: data['name'] ?? '',
+      email: data['email'] ?? '',
+      contact: data['contact'] ?? '',
+      bio: data['bio'] ?? '',
     );
   }
 }
